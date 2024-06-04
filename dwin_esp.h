85f0fdea0d708a04ef65b09555acaf3ebef86003
Mahmoud Megahd <megomego354@gmail.com>
2024-06-04 08:13:27 +0300

    Update dwin_esp.h

--- dwin_esp.h
+++ dwin_esp.h
@@ -11,15 +11,15 @@ void GO_PAGE(uint16_t PG){
 }
 
 
-void ERASE_TEXT(int high_address, long d_Len){ 
+void ERASE_TEXT(int high_address, long Len){ 
   Serial.write(0x5A); 
   Serial.write(0xA5); 
-  Serial.write(d_Len + 3); 
+  Serial.write(Len + 3); 
   Serial.write(0x82);
 
   Serial.write(high_address);
   Serial.write(0x00); // second
-  for (int t = 0; t < d_Len; t++) {
+  for (int t = 0; t < Len; t++) {
     Serial.write(0x20);
   }
 }
@@ -37,25 +37,4 @@ void SET_TEXT(int high_address, String textData){
   {
     Serial.write(textData[i]);
   }
-}
-
-
-void error_case(String error, int pic_id){
-    GO_PAGE(2);
-    ERASE_TEXT(0x50, error_length);
-    if (error.length() > 100) {
-        error = error.substring(0, 100);
-    }
-    error_length = error.length();
-    SET_TEXT(0x50, error);
-    GO_PAGE(pic_id);
-    delay(1000 * 5);
-    GO_PAGE(2);
-    
-    ERASE_TEXT(0x50, error_length);
-    SET_TEXT(0x50, "loding");
-    error_length = 6;
-    delay(1000);
-
-    GO_PAGE(pic_id);
 }
